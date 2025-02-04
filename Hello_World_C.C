a34b00d660ea7f1b5e070aaf936222a37e4ab93c
Magezi Richard Elijah <magezirichardelijah@gmail.com>
2025-02-04 19:08:18 +0300

    Add files via upload

---
+++ Hello_World_C.C
@@ -0,0 +1,10 @@
+#include <stdio.h>//This is to include the library stdio.h to input not put to input text to the terminal
+
+int main(void)//Defining a function called main for integers but void makes sure it doesnt take any arguements/parameters
+{
+    printf("Hello World!\n");
+    return 0;
+}
+//return 0 is the standard return value from the main function. Its telling the system that the program returned with a status of zero.
+//printf is calling the function(telling the function to do some work for us). In this case output to the screen hello world.
+//semi colons end statements
