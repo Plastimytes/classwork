42097598a1930bf161f313efb18bf799bfa355fc
Plastimytes <magezirichardelijah@gmail.com>
2025-02-12 19:09:40 +0300

    Update if_2.c

--- C stuff/Week_Two/if_2.c
+++ C stuff/Week_Two/if_2.c
@@ -4,7 +4,7 @@ int main(void){
  
     if (x<4)printf("1st cond!\n");
     else if(x<5)printf("2nd cond!\n");
-    else if(x<6)printf("3rd cond!\n");
+    else if(x<6)printf("3rd cond!\n");//Once the condition is satisfied, the code stops checking the queries and jumps down to the if done print statement
     else if(x<7)printf("4th cond!\n");
     else printf("else case!\n");
 
