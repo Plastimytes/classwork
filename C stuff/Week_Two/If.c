fdbb77373b129c573566e1d259faeec9748db5e1
Plastimytes <magezirichardelijah@gmail.com>
2025-02-12 18:48:56 +0300

    basic if statemennts

---
+++ C stuff/Week_Two/If.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    int x = 3;
+    if(x == 2)
+    {printf("x IS EQUAL TO 2 !!\n");}//The semicolon is inside the curly bracket here
+    else{printf("x IS not EQUAL TO\n");}
+    printf("if done!\n");
+    return 0;
+}
---
+++ C stuff/Week_Two/nrt.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int c= printf("Give a name: ");
+    scanf("%s",&c);
+    printf("Name: c\n", c);
+return 0;
+
+}
