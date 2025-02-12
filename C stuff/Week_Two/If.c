9fb9ed9d87a4ba5cd35d0b0ec7505765b06cd9bf
Plastimytes <magezirichardelijah@gmail.com>
2025-02-12 18:54:30 +0300

    binary file

--- C stuff/Week_Two/If.c
+++ C stuff/Week_Two/If.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
 int main(void){
-    int x = 3;
+    int x = 5;
     if(x == 2)
-    {printf("x IS EQUAL TO 2 !!\n");}//The semicolon is inside the curly bracket here
-    else{printf("x IS not EQUAL TO\n");}
+    {
+        printf("x IS EQUAL TO 2 !!\n");
+    }//The semicolon is inside the curly bracket here
+    else if(x == 3){
+        printf("x IS EQUAL TO 3 !!\n");
+    }
+    else{
+        printf("x IS not EQUAL TO 2 or 3\n");
+    }
     printf("if done!\n");
     return 0;
 }
