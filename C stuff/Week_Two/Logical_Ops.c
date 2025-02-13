a65b1130879066466d4ac1157ea54df5d445e4f0
Plastimytes <magezirichardelijah@gmail.com>
2025-02-13 19:06:03 +0300

    logical operators

---
+++ C stuff/Week_Two/Logical_Ops.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    int x=5;
+    if(x>5) printf("OK!\n");
+    else printf("Not OK!\n");
+    
+    int year = 2019;
+    int price = 21000;
+
+    if (year >= 2019 && price <= 20000)printf("OK!\n");
+    else printf("Not Ok!\n");
+    return 0;
+}
+
