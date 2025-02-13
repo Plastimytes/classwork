e558a4730456043e7df7524e09624bbeacaa0141
Plastimytes <magezirichardelijah@gmail.com>
2025-02-13 19:09:29 +0300

    And operators edit

--- C stuff/Week_Two/Logical_Ops.c
+++ C stuff/Week_Two/Logical_Ops.c
@@ -4,9 +4,11 @@ int main(void){
     int x=5;
     if(x>5) printf("OK!\n");
     else printf("Not OK!\n");
-    
+
+    ///AND operators(&&)
+
     int year = 2019;
-    int price = 21000;
+    int price = 20000;
 
     if (year >= 2019 && price <= 20000)printf("OK!\n");
     else printf("Not Ok!\n");
