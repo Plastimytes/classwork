a11641f9310e46fc54967fb82a53fbe3c1575d23
Plastimytes <magezirichardelijah@gmail.com>
2025-02-18 18:06:47 +0300

    assuming

--- C stuff/Week_Three/Min_Array.c
+++ C stuff/Week_Three/Min_Array.c
@@ -5,7 +5,12 @@
 
 int main(void)
 {
-    int myarray[] = {2,3,6,1,8,5,10,34,24}
+    int myarray[] = {2,3,6,1,8,5,10,34,24};
+
+    //Finding the minimum number in the array
+    int min;
+    min = myarray[0]; //Assume that the first thing in tthe array is the smallest thing in the array..For now
+    
 
     return 0;
 }
