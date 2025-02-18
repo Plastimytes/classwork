d57a35764facc278b442ab8d911309966d6a2bc9
Plastimytes <magezirichardelijah@gmail.com>
2025-02-18 18:36:41 +0300

    Update Min_Array.c

--- C stuff/Week_Three/Min_Array.c
+++ C stuff/Week_Three/Min_Array.c
@@ -3,21 +3,25 @@
 
 ///  Exact type doesn't really matter cause its almost exactly the same
 
+//Create function
+int find_min(int myarray[], int length);
 int main(void)
 {
     int myarray[] = {2,3,6,1,8,5,10,34,24};
+    return 0;
+}
 
-    //Finding the minimum number in the array
+int find_min(int myarray[], int length){
     int min;
     min = myarray[0]; //Assume that the first thing in the array is the smallest thing in the array..For now
-    for (int i = 0; i < 9; i++)///9 is the number of things in the array
+    for (int i = 0; i < length; i++);
     {
-        printf ("Checkin myarray [%d] = %d\n", i, myarray[i]);
+
+        //printf ("Checkin myarray [%d] = %d\n", i, myarray[i]);
+
         if (myarray[i]< min) min = myarray[i];//if my array at i is less than min, then make min equal to myarray at i
         printf("New min found: %d\n", min);
+        printf("Minimum: %d\n", min);
     }
-
-    
-    printf("Minimum: %d\n", min);
-    return 0;
+    return min;
 }
