0f49cbb716b5b8b2c5120586ec8bc16412a97979
Plastimytes <magezirichardelijah@gmail.com>
2025-02-18 18:12:59 +0300

    Update Min_Array.c

--- C stuff/Week_Three/Min_Array.c
+++ C stuff/Week_Three/Min_Array.c
@@ -10,7 +10,11 @@ int main(void)
     //Finding the minimum number in the array
     int min;
     min = myarray[0]; //Assume that the first thing in tthe array is the smallest thing in the array..For now
+    for (int i = 0; i < 9; i++)///9 is the number of things in the array
+    {
+        if (myarray[i]< min) min = myarray[i];//if my array at i is less than min, then make min equal to myarray at i
+    }
     
-
+    printf("Minimum: %d\n", min);
     return 0;
 }
