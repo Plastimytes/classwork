17c4c037c624854f5cc472277cdff530da388995
Plastimytes <magezirichardelijah@gmail.com>
2025-02-24 17:19:49 +0300

    Update Classes and obects.py

--- Object Orieted Programming/Classes and obects.py
+++ Object Orieted Programming/Classes and obects.py
@@ -3,14 +3,19 @@
 #In a class you can set your attribute(variables) and behaviour(Functions/methods)
 
 class Computer:
-  def config(self):#config is the method and self just appears
+  def config(self):#config is the method and self just appears. Self is the object which your passing
     print("I5, 16GB, 1TB")
 
+class Computer2:
+  def config(self):#config is the method and self just appears. Self is the object which your passing
+    print("I4, 6GB, 500gB")
 
 COM1 = Computer()#This...() is a constructor    
+COM2 = Computer2 ()
 
 #Calling the function/method... Mention the name of the class then .config. If yuo want to use a method this what you do.
 Computer.config(COM1)
+Computer2.config(COM2)
 
 #X=5
 
