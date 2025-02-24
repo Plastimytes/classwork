d0f1d7be0b9dfb93e6dd69a6f1af51c10e89a228
Plastimytes <magezirichardelijah@gmail.com>
2025-02-24 16:58:32 +0300

    Create Classes and obects.py

---
+++ Object Orieted Programming/Classes and obects.py
@@ -0,0 +1,10 @@
+###If you want an object you need to create a class
+##Defining a class
+#In a class you can set your attribute(variables) and behaviour(Functions/methods)
+
+class Computer:
+  def config(self):#config is the method and self just appears
+    print("I5, 16GB, 1TB")
+
+
+COM1 = Computer()#This is a constructor    
