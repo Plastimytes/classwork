3d59ce96464493aed12a03b48651e36d96e7b8bf
Plastimytes <magezirichardelijah@gmail.com>
2025-02-24 17:44:10 +0300

    Update init.py

--- Object Orieted Programming/init.py
+++ Object Orieted Programming/init.py
@@ -4,7 +4,7 @@ class Computer:
     self.CPU = CPU
     self.RAM = RAM #RAM is an attribute of the class, it's a property of the object. It's a variable that belongs to an object.
   def config(self):#config is the method and self just appears. Self is the object which your passing
-    print("Config is", self.CPU, "and ",self.RAM, "GB")# Its just how you should do it
+    print("Config is", self.CPU, "and ",self.RAM, "GB")#yOU HAVE TO COME AND WRITE THIS IN THE CONFIG. Its just how you should do it
 
 #Passing arguements
 COM1 = Computer('I5', 16)
