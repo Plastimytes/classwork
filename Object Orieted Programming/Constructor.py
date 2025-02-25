0780467fbf15d8303f4fd8c7b15c96787a001f36
Plastimytes <magezirichardelijah@gmail.com>
2025-02-25 16:50:47 +0300

    Update Constructor.py
    Comparing values

--- Object Orieted Programming/Constructor.py
+++ Object Orieted Programming/Constructor.py
@@ -9,6 +9,12 @@ class computer: #If the class is empty it will not work
       self.name = "Jane"
       self.age = 22
 
+   def compare(self, other):
+      if self.age ==  other.age:
+         return True 
+      else:
+         return False 
+
 c1 = computer()#computer() is the constructor
 #creating another object(it will create different addresses)
 c2 = computer()
@@ -20,9 +26,22 @@ c2.age=23
 #Updating the object values
 c1.update()
 
+ 
+
+#Comparing a value n the object
+if c1.compare(c2):
+   print("Same")
+
 #Creating an id function to print the address of c1
 print(c1.name) 
 print(c2.name ) 
 print("......")
 print(c1.age) 
 print(c2.age ) 
+
+#Self is a pointer / director
+#Comparing
+if c1 == c2:
+   print("Same")
+else:
+   print("Different")  
