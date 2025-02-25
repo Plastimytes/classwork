f03a21e095a474e2da70bec12746d03fda4e117b
Plastimytes <magezirichardelijah@gmail.com>
2025-02-25 17:04:10 +0300

    Update class_car.py
    withdraw

--- Object Orieted Programming/Assignments/class_car.py
+++ Object Orieted Programming/Assignments/class_car.py
@@ -19,9 +19,18 @@ class car:
                      self.balance += amount
                      print(f"Deosited {amount}. New balance is {self.balance}")
                  else:
-                     print("Invalid deposit amount")   
+                     print("Invalid deposit amount")
+
+           def withdraw(self, amount):
+                if amount > 0 and amount <= self.balance:
+                     self.balance -= amount
+                     print(f"Withdrew {amount}. New balance is {self.balance}")
+                else:
+                     print("Invalid withdrawal amount or insufficient funds")             
         Call3=bank_account("qdgfr242353", 2300000)
         Call3.config()
+
+        
     def config(self):
         print("The car make is ",self.make, "of the model",self.model, "and year", self.year)
 
