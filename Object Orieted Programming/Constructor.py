058c34749d6e838368a126475dd5d27fdab311d3
Plastimytes <magezirichardelijah@gmail.com>
2025-02-25 16:18:42 +0300

    Update Constructor.py

--- Object Orieted Programming/Constructor.py
+++ Object Orieted Programming/Constructor.py
@@ -4,6 +4,9 @@ class computer: #If the class is empty it will not work
    pass
 
 c1 = computer()
+#creating another object(it will create different addresses)
+c2 = computer()
 
 #Creating an id function to print the address of c1
-print(id(c1))
+print(id(c1)) 
+print(id(c2)) 
