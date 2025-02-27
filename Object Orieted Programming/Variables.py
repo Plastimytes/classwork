082e485f2dc0377fa20a025eac1bca75b098daa7
Plastimytes <magezirichardelijah@gmail.com>
2025-02-27 19:02:46 +0300

    Update Variables.py

--- Object Orieted Programming/Variables.py
+++ Object Orieted Programming/Variables.py
@@ -1,4 +1,6 @@
 class Car:
+
+    wheels = 4
     def __init__(self):
         self.mil = 10
         self.com = "BMW"
@@ -7,6 +9,7 @@ class Car:
 c1 = Car()
 c2 = Car()
 
+c1.mil = 8
 
 print(c1.mil, c1.com)
 print(c2.mil, c2.com)
