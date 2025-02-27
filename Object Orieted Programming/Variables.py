de61939d0b3c8d99c7d23dc2b58ba7ed112e6f5f
Plastimytes <magezirichardelijah@gmail.com>
2025-02-27 19:06:15 +0300

    Update Variables.py

--- Object Orieted Programming/Variables.py
+++ Object Orieted Programming/Variables.py
@@ -12,5 +12,6 @@ c2 = Car()
 c1.mil = 8
 ###Note: Outside the init function you have class variables and inside you have instance variables
 
+Car.wheels =5
 print(c1.mil, c1.com)
 print(c2.mil, c2.com)
