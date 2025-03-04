f653dad17439d5e9ca498d285952eab371e2e316
Plastimytes <magezirichardelijah@gmail.com>
2025-03-04 18:15:20 +0300

    Update TypesMethod.py

---
+++ Object Orieted Programming/TypesMethod.py
@@ -0,0 +1,12 @@
+###Types of Methods
+### Instance methods, Class methods and static methods
+
+class Student:
+    def __init__(self,m1,m2,m3):
+        self.m1 = m1
+        self.m2 = m2
+        self.m3 = m3
+
+
+
+#Ojects        
