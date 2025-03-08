587b66a90739577ceb6eb6a5420ea234a70ae3e8
Plastimytes <magezirichardelijah@gmail.com>
2025-03-08 09:46:53 +0300

    Update app.py

--- praction/APPS/Notes_App/app.py
+++ praction/APPS/Notes_App/app.py
@@ -7,3 +7,15 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'content': self.content
+        }
