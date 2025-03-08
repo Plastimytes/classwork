306aeaf7321a52b3608975e153e8e59f23e963c2
Plastimytes <magezirichardelijah@gmail.com>
2025-03-08 09:45:54 +0300

    Update app.py

--- praction/APPS/Notes_App/app.py
+++ praction/APPS/Notes_App/app.py
@@ -1,4 +1,9 @@
 #app.py
 
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
