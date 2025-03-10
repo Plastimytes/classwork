e9003363ae26ce14e0a3b2639fc6e3a808e4c7e7
Plastimytes <magezirichardelijah@gmail.com>
2025-03-10 16:56:01 +0300

    Update inner_class.py
    Inner classes

--- Object Orieted Programming/inner_class.py
+++ Object Orieted Programming/inner_class.py
@@ -3,8 +3,22 @@ class Student:
     def __init__(self, name, rollno):
         self.name = name
         self.rollno =rollno
+        self.lap =self.Laptop()#For the inner class
+
+    def show(self):#Goes to s1.show
+        print(self.name, self.rollno) 
+
+    #INNER CLASS
+    class Laptop:
+        def __init__(self, brand, model, cpu, ram):
+            self.brand = 'HP'
+            self.model = 'Pavilion' 
+            self.cpu = "i5"
+            self.ram = '8GB'   
 
 s1 =Student('Richie',2)  
 s2 =Student('Jane', 3)
 
 print(s1.name, s1.rollno)
+
+s1.show()#To give all the values of s1
