51bed10b12e2d601950f5a881c5d216b09b50e34
Plastimytes <magezirichardelijah@gmail.com>
2025-03-10 16:40:22 +0300

    Create inner_class.py

---
+++ Object Orieted Programming/inner_class.py
@@ -0,0 +1,10 @@
+class Student:
+
+    def __init__(self, name, rollno):
+        self.name = name
+        self.rollno =rollno
+
+s1 =Student('Richie',2)  
+s2 =Student('Jane', 3)
+
+print(s1.name, s1.rollno)
