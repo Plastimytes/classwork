f938a8adaf8ab2cd57a00ca62a3e1a5ca0c55f23
Plastimytes <magezirichardelijah@gmail.com>
2025-03-11 20:16:45 +0300

    Update inheritance.py

--- Object Orieted Programming/inheritance.py
+++ Object Orieted Programming/inheritance.py
@@ -5,4 +5,12 @@ class A:
         print("Feature One is working...")
 
     def feature2(self):
-        print("Feature Two is working...")        
+        print("Feature Two is working...")
+
+
+#Objects
+a1 =A()    
+
+#Calling the methods (feature1, feature2)
+a1.feature1()
+a1.feature2()
