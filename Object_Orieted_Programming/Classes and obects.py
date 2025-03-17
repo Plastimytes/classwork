36fe3c38b1a6be7ae7ac8480eb81f5711f71b549
Plastimytes <magezirichardelijah@gmail.com>
2025-03-17 17:14:05 +0300

    Update Classes and obects.py

--- Object_Orieted_Programming/Classes and obects.py
+++ Object_Orieted_Programming/Classes and obects.py
@@ -1,7 +1,6 @@
 ###If you want an object you need to create a class
 ##Defining a class
 #In a class you can set your attribute(variables) and behaviour(Functions/methods)
-
 class Computer:
   def config(self):#config is the method and self just appears. Self is the object which your passing
     print("I5, 16GB, 1TB")
