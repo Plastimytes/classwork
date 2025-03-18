f75d18adccf812ccd5a9931b7d32b08943d03f83
Plastimytes <magezirichardelijah@gmail.com>
2025-03-18 19:22:02 +0300

    Update Polymorphism.py

--- Object_Orieted_Programming/Polymorphism.py
+++ Object_Orieted_Programming/Polymorphism.py
@@ -1,3 +1,28 @@
 ###Concept is use din Loose coupling, dependency injection, interfaces
 ##Four ways of implementaing polymoriphism: Duck Typing, Operator overloading, Method overloading, Method overriding
-##
+##Duck Typing Example:
+class Duck:
+    def quack(self):
+        return "Quack!"
+
+    def walk(self):
+        return "Waddle waddle!"
+
+class RobotDuck:
+    def quack(self):
+        return "Beep beep quack!"
+
+    def walk(self):
+        return "Rolling forward!"
+
+def make_it_quack_and_walk(duck_like_thing):
+    print(duck_like_thing.quack())
+    print(duck_like_thing.walk())
+
+# Create instances of Duck and RobotDuck
+real_duck = Duck()
+robot_duck = RobotDuck()
+
+# Both can be passed to the same function
+make_it_quack_and_walk(real_duck)   # This is a real duck
+make_it_quack_and_walk(robot_duck)  # This is a robot duck
