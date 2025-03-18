37a841cfd35306ec1788c8ed9fa706b07658ac95
Plastimytes <magezirichardelijah@gmail.com>
2025-03-18 19:05:19 +0300

    Update Polymorphism.py

--- Object_Orieted_Programming/Polymorphism.py
+++ Object_Orieted_Programming/Polymorphism.py
@@ -1,1 +1,2 @@
-###Concept is use din Loose coupling, dependency injection 
+###Concept is use din Loose coupling, dependency injection, interfaces
+##Four ways of implementaing polymoriphism: Duck Typing, Operator overloading, Method overloading
