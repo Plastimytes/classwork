42586fa36931d4d7c762b2a42ca26ebe5e137fa0
Plastimytes <magezirichardelijah@gmail.com>
2025-04-09 09:29:21 +0300

    Update Basics.py

---
+++ Computational Maths/Basics.py
@@ -0,0 +1,26 @@
+#Numbers
+x=11
+y=2
+
+#Addition
+add=x+y
+#Subtraction
+sub=x-y
+#Multiplication
+mul=x*y
+#Division(float)
+divft=x/y
+#Division(floor)
+divfr=x//y
+#Division(Modular)
+divmr=x%y
+#Power
+pow=x**y
+
+#Print results
+print("Addition=", add)
+print("Subtraction=", sub)
+print("Multiplication=", mul)
+print("Division(float)=", divft)
+print("Division(Modular)=", divfr)
+print("Power=", pow)
