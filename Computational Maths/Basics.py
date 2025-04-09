eeb94943910fd3456457ace6cc4f72de904bb265
Plastimytes <magezirichardelijah@gmail.com>
2025-04-09 09:45:48 +0300

    Using modules and functions

--- Computational Maths/Basics.py
+++ Computational Maths/Basics.py
@@ -22,5 +22,11 @@ print("Addition=", add)
 print("Subtraction=", sub)
 print("Multiplication=", mul)
 print("Division(float)=", divft)
-print("Division(Modular)=", divfr)
+print("Division(floor)=", divfr)
+print("Division(Modular)=", divmr)
 print("Power=", pow)
+
+
+## Computing the variables a, b, c in one line of code using the separation operator ;
+a=1+2*3; b=(1+2)*3; c=2**2*3
+print(a,b,c)
---
+++ Computational Maths/Mathmodlues.py
@@ -0,0 +1,18 @@
+import math
+print(math.sin(math.pi/5))
+
+#If you want to write less
+from math import*
+print(sin(pi/5)
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          ))
