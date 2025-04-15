7e43c45d09d853cededcf6378e134b41b625e5fe
Plastimytes <magezirichardelijah@gmail.com>
2025-04-15 19:15:07 +0300

    chanes mADE

--- Computational Maths/dictonaries.py
+++ Computational Maths/dictonaries.py
@@ -5,3 +5,8 @@ vars={\
             }
 
 print(vars)##The backslashes arent really neccesary
+
+#We can change the value of a key in a dictionary by setting the key of the dictionary variable equal to the new value:
+vars["y"]=10
+print(vars["y"])
+print(vars)
---
+++ Computational Maths/lists.py
@@ -0,0 +1,9 @@
+list=[5,3,7]
+print(list[0])
+print(list[1])
+print(list[2])
+
+print(list[-1])
+
+list[1] = 9
+print(list)
