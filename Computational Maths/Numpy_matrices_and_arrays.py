0be77d2f273d3169ea75867eb162949c85dbbe45
Plastimytes <magezirichardelijah@gmail.com>
2025-05-14 09:06:09 +0300

    shapes of matrices

---
+++ Computational Maths/Numpy_matrices_and_arrays.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+#Shape of the matrix
+a=np.array([[1,2,3]])
+print(a.shape)
--- Computational Maths/Tester.py
+++ Computational Maths/Tester.py
@@ -2,5 +2,8 @@ import numpy as np
 B = np.array( [[1, 2, 3],
  [4, 5, 6],
  [7, 8, 9]] )
-C = B[0:1, 0:1]
+C = B[0:2, 0:2]
 print(C)
+
+#Shape of the matrix
+print(B.shape)
