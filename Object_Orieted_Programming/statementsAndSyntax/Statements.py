6576696991004605733ca986e3bd452c66741d18
Plastimytes <magezirichardelijah@gmail.com>
2025-06-16 08:49:41 +0300

    Update Statements.py

--- Object_Orieted_Programming/statementsAndSyntax/Statements.py
+++ Object_Orieted_Programming/statementsAndSyntax/Statements.py
@@ -9,3 +9,18 @@ while True:
  if ans == 'stop': break
  print(int(ans) ** 2)
 print('Bye')
+
+##Example 3: Nesting Code Three Levels Deep
+while True:
+ reply = input('Enter text:')
+ if reply == 'stop':break
+ elif not reply.isdigit():
+  print('Bad!' * 8)
+ else:
+  num = int(reply)
+ if num < 20:
+  print('low')
+ else:
+  print(num ** 2)
+print('Bye')
+
