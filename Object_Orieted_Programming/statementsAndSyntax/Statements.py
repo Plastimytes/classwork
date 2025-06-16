40cb38bdfe4efebae3922259fb65169136272684
Plastimytes <magezirichardelijah@gmail.com>
2025-06-16 08:42:32 +0300

    Update Statements.py

--- Object_Orieted_Programming/statementsAndSyntax/Statements.py
+++ Object_Orieted_Programming/statementsAndSyntax/Statements.py
@@ -3,4 +3,9 @@ while True:
  if reply == 'stop': break
  print(reply.upper())
 
-##Example2
+##Example2:Math and user inputs
+while True:
+ ans = input('Enter text:')
+ if ans == 'stop': break
+ print(int(ans) ** 2)
+print('Bye')
