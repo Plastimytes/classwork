852b32774ae30b7652b276742cd9a92f995fefaa
Plastimytes <magezirichardelijah@gmail.com>
2025-06-19 08:45:59 +0300

    Create npc.py

---
+++ Object_Orieted_Programming/Assignments/npc.py
@@ -0,0 +1,11 @@
+#Importing
+import pygame
+import sys
+
+#Initalizing pygame
+pygame.init()
+
+#Dimensions on theb screeen
+Screen_width = 720
+Screen_height = 1080
+screen = pygame.display.set_mode((Screen_width, Screen_height))
