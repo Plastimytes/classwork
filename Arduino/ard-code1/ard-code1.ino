a053e80a3501ebd68806a92b83411e5a7b0a1bec
Magezi Richard Elijah <magezirichardelijah@gmail.com>
2025-11-05 16:33:45 +0300

    Update ard-code1.ino

--- Arduino/ard-code1/ard-code1.ino
+++ Arduino/ard-code1/ard-code1.ino
@@ -8,6 +8,7 @@ void loop()
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(11, HIGH);
   delay(1000); // Wait for 90 millisecond(s)
+
   // turn the LED off by making the voltage LOW
   digitalWrite(11, LOW);
   delay(1000); // Wait for 90 millisecond(s)
