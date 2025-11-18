caaaff6b7acbbb28ef456fe05e82d0ac036033af
Magezi Richard Elijah <magezirichardelijah@gmail.com>
2025-11-18 11:53:41 +0300

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,24 @@
+## Tuple-Immutable list of ordered data types. You cant append tuples
+
+
+#Lists
+names = ["Anna", "Brian", "Cathy"]
+print(f"LIST EXAMPLE: {names}")
+
+#Append
+names.append("Jeremy")
+
+
+#Tuples
+student_info = ("S01", "Anna")
+print(f"Student: {student_info}")
+
+#Dictionary. Key and values (Word and what it means)
+
+student_engine = {
+    "Anna": "Wankel",
+    "Danny": "Honda",
+    "Jeremy": "2JZ"
+}
+
+print(f"Dict_Example:{student_engine}")
